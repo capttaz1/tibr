80ea334260f4cae5385476a936ee8cd348612d56
Rob Morris <capttaz@gmail.com>
2025-04-27 14:57:41 -0400

    latest update

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -36,86 +36,84 @@ export async function initHandler(name: string, preset: 'react-monorepo' | 'ts',
 		[
 			'create-nx-workspace@latest',
 			name,
-			`--preset=apps`,
-			// `--packageManager=${pm}`,
-			// '--interactive=false',
-			// '--ci=skip',
+			`--preset=${preset}`,
+			`--packageManager=${pm}`,
+			'--interactive=false',
+			'--nxCloud=false',
 		],
 		{ stdio: 'inherit' }
 	);
 
-	// cd into new workspace
+	// change into new workspace
 	process.chdir(workspaceDir);
 
-	// // 3) install & init all of our plugins
-	// console.log('🔧 Installing & initializing Nx plugins…');
-	// await execa(pm, ['install', '-D', '@nx/express', '@nx/react', '@nx/storybook', '@nx/cypress'], {
-	// 	stdio: 'inherit',
-	// });
-	// await execa('nx', ['add', '@nx/storybook', '--interactive=false'], { stdio: 'inherit' });
+	// 3) install & initialize Nx plugins
+	console.log('🔧 Installing & initializing Nx plugins…');
+	await execa(pm, ['install', '-D', '@nx/express', '@nx/react', '@nx/storybook', '@nx/cypress'], {
+		stdio: 'inherit',
+	});
+	await execa('nx', ['add', '@nx/storybook', '--interactive=false'], { stdio: 'inherit' });
+	await execa('nx', ['generate', '@nx/express:init'], { stdio: 'inherit' });
+	await execa('nx', ['generate', '@nx/react:init'], { stdio: 'inherit' });
+	await execa('nx', ['generate', '@nx/storybook:init'], { stdio: 'inherit' });
+	await execa('nx', ['generate', '@nx/cypress:init'], { stdio: 'inherit' });
 
-	// await execa('nx', ['generate', '@nx/express:init'], { stdio: 'inherit' });
-	// await execa('nx', ['generate', '@nx/react:init'], { stdio: 'inherit' });
-	// // await execa('nx', ['generate', '@nx/storybook:init'], { stdio: 'inherit' });
-	// await execa('nx', ['generate', '@nx/cypress:init'], { stdio: 'inherit' });
+	// 4) generate our React & Express apps
+	console.log('🔨 Generating React client app…');
+	await execa(
+		'nx',
+		[
+			'generate',
+			'@nx/react:application',
+			'client',
+			'--directory=apps',
+			'--routing',
+			'--style=css',
+			'--unitTestRunner=jest',
+			'--e2eTestRunner=none',
+			'--linter=eslint',
+			'--no-interactive',
+		],
+		{ stdio: 'inherit' }
+	);
 
-	// // 4) generate our React & Express apps
-	// console.log('🔨 Generating React client app…');
-	// await execa(
-	// 	'nx',
-	// 	[
-	// 		'generate',
-	// 		'@nx/react:application',
-	// 		'client',
-	// 		'--directory=apps',
-	// 		'--routing',
-	// 		'--style=css',
-	// 		'--unitTestRunner=jest',
-	// 		'--e2eTestRunner=none',
-	// 		'--linter=eslint',
-	// 		'--no-interactive',
-	// 	],
-	// 	{ stdio: 'inherit' }
-	// );
+	console.log('🔨 Generating Express API app…');
+	await execa('nx', ['generate', '@nx/express:application', 'api', '--directory=apps', '--no-interactive'], {
+		stdio: 'inherit',
+	});
 
-	// console.log('🔨 Generating Express API app…');
-	// await execa('nx', ['generate', '@nx/express:application', 'api', '--directory=apps', '--no-interactive'], {
-	// 	stdio: 'inherit',
-	// });
+	// 5) generate our UI library under libs/ui
+	console.log('🔨 Generating UI library…');
+	await execa(
+		'nx',
+		[
+			'generate',
+			'@nx/react:library',
+			'ui',
+			'--directory=libs',
+			'--style=css',
+			'--unitTestRunner=jest',
+			'--linter=eslint',
+			'--bundler=none',
+			'--no-interactive',
+		],
+		{ stdio: 'inherit' }
+	);
 
-	// // 5) generate our UI library under libs/ui
-	// console.log('🔨 Generating UI library…');
-	// await execa(
-	// 	'nx',
-	// 	[
-	// 		'generate',
-	// 		'@nx/react:library',
-	// 		'ui',
-	// 		'--style=css',
-	// 		'--unitTestRunner=jest',
-	// 		'--linter=eslint',
-	// 		'--bundler=none',
-	// 		'--no-interactive',
-	// 	],
-	// 	{ stdio: 'inherit' }
-	// );
-
-	// // 6) configure Storybook **for the “ui” project**
-	// console.log('🔨 Configuring Storybook for UI library…');
-	// // await execa(
-	// // 	'nx',
-	// // 	[
-	// // 		'generate',
-	// // 		'@nx/storybook:configuration',
-	// // 		'ui', // ← now matches the actual project name
-	// // 		'--uiFramework=@storybook/react-webpack5',
-	// // 		'--generateCypressSpecs=false',
-	// // 		'--no-interactive',
-	// // 	],
-	// // 	{ stdio: 'inherit' }
-	// // );
-
-	// await execa('nx', ['g', '@nx/storybook:configuration', 'ui'], { stdio: 'inherit' });
+	// 6) configure Storybook for the “ui” project
+	console.log('🔨 Configuring Storybook for UI library…');
+	await execa(
+		'nx',
+		[
+			'generate',
+			'@nx/react:storybook-configuration',
+			'ui',
+			'--uiFramework=@storybook/react-webpack5',
+			'--generateCypressSpecs=false',
+			'--no-interactive',
+		],
+		{ stdio: 'inherit' }
+	);
 
 	console.log('✅ Done! Your workspace is ready.');
 }
